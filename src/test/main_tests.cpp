24e1f257b3f143a272bd88ff82c7d61fb02486f7
Unat <unattainiumcoin@gmail.com>
2015-04-22 22:51:59 +0200

    4
    4

--- src/test/main_tests.cpp
+++ src/test/main_tests.cpp
@@ -10,10 +10,10 @@
 BOOST_AUTO_TEST_SUITE(main_tests)
 
 BOOST_AUTO_TEST_CASE(subsidy_limit_test)
-{
+{ 
     uint64_t nSum = 0;
     for (int nHeight = 0; nHeight < 14000000; nHeight += 1000) {
-        uint64_t nSubsidy = GetBlockValue(nHeight, 0);
+        uint64_t nSubsidy = GetBlockValue(nHeight, 0, uint256 prevHash);
         BOOST_CHECK(nSubsidy <= 50 * COIN);
         nSum += nSubsidy * 1000;
         BOOST_CHECK(MoneyRange(nSum));
